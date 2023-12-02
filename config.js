7383dfd39c01bbd5e0a1ab5aecde3954dad0f14e
ᴋɪᴅ ꜱʟᴀꜱʜᴇʀ <87477295+JoyBoySer@users.noreply.github.com>
2023-12-02 22:05:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
+  ['918714504817', 'Dinuwa Official²⁰²³', true],
   ['94767939688', 'Dinuwa Official²⁰²³', true], 
   [''] 
 ] //Number of owners
